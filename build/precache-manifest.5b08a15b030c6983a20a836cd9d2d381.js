a4c908513130d8fe61348496246390af4a0a1f49
yahya <yahyamohandessam@gmail.com>
2022-01-22 11:53:27 +0300

    build

---
+++ build/precache-manifest.5b08a15b030c6983a20a836cd9d2d381.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff150523c4d606f694ed5554502b45f7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "147d662f59a821b1dd10",
+    "url": "/static/css/2.87b44db9.chunk.css"
+  },
+  {
+    "revision": "d00d29b25c184d158af8",
+    "url": "/static/css/main.c4969ba3.chunk.css"
+  },
+  {
+    "revision": "147d662f59a821b1dd10",
+    "url": "/static/js/2.b690e1bc.chunk.js"
+  },
+  {
+    "revision": "0033db3dd82d6cbfb6f733e3747037bc",
+    "url": "/static/js/2.b690e1bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d00d29b25c184d158af8",
+    "url": "/static/js/main.682be6a6.chunk.js"
+  },
+  {
+    "revision": "e44798faaee9b4b27aae",
+    "url": "/static/js/runtime-main.d276b5a1.js"
+  },
+  {
+    "revision": "1cedb6e919bfed6a2c1ec00b5d8ee620",
+    "url": "/static/media/UploadIcon.1cedb6e9.svg"
+  }
+]);
